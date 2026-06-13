32bd0149d7215ba4b5e80bf7adc3cb63f8d54b9e
Gaurav Sunil Bagul <gauravsbagul@gmail.com>
2026-06-14 00:57:32 +0530

    Implement file/folder addition in FileExplorer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
 import "./styles.css";
 import { FileExplorer } from "./container/FileExplorer";
-import { useEffect } from "react";
+import { useState } from "react";
 import { filesAndFolders } from "./data";
+
+const addObjectAtDepth = (list, targetDepth, newObj, currentDepth = 0) => {
+  if (!list || !Array.isArray(list)) return list;
+
+  if (currentDepth === targetDepth) {
+    list.push(newObj);
+    return list;
+  }
+
+  // Traverse deeper into children
+  list.forEach((node) => {
+    if (node.children) {
+      const list = addObjectAtDepth(
+        node.children,
+        targetDepth,
+        newObj,
+        currentDepth + 1
+      );
+    }
+  });
+  return list;
+};
+
 export default function App() {
+  const [list, setList] = useState(filesAndFolders);
+
+  const onAddNewFileOrFlder = (option) => {
+    console.log("option", option);
+    const { parentFolder, index, name, level, isFile } = option;
+    const tempList = JSON.parse(JSON.stringify(list));
+
+    const newList = addObjectAtDepth(tempList, level, {
+      name: `New ${name}`,
+      isFile,
+    });
+    console.log("newList ~->", newList);
+    setList(newList);
+  };
+
   return (
     <div className="App">
-      <FileExplorer list={filesAndFolders} />
+      <div className="sideBar">
+        <FileExplorer
+          list={filesAndFolders}
+          onAddNewFileOrFlder={onAddNewFileOrFlder}
+          level={0}
+        />
+      </div>
     </div>
   );
 }
